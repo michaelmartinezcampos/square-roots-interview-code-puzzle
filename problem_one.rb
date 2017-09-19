42cd4e3744a1d44e71e74a09a00636fea022360f
Paul Berry <pdbthefifth@gmail.com>
2017-09-19 18:29:38 -0400

    problems 1 and 2

---
+++ problem_one.rb
@@ -0,0 +1,8 @@
+# ===================================================================================
+#     Print a list of location names, sorted by weekly revenue in descending order
+# ===================================================================================
+# => Read from locations.json and customers.json files.
+# => The plan attribute on a customer is the number of leafy green packs 
+#    that they receive each week. Each pack costs $5.
+# => Do not count customers with status "unsubscribed" towards revenue.
+# => Feel free to use any library you'd like.
---
+++ problem_two.rb
@@ -0,0 +1,7 @@
+# =====================================================================================================
+#     Print a list of location names, ordered from shortest to longest driving distance from the farm
+# =====================================================================================================
+# => Read from locations.json and customers.json files.
+# => The Square Roots farm is located at 630 Flushing Ave, Brooklyn NY 11211.
+# => Feel free to use any library you'd like.
+
